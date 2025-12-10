b07c19584e2ba4951f9378fd2b30b7d068d5ea84
muiz25 <ijadunolamuizz@gmail.com>
2025-12-10 10:24:11 +0100

    new

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './app.css';
 import Navbar from './Components/Navbar/Navbar';
 import Projects from './Components/Projects/Projects';
 import Skill from './Components/Skill/Skill';
